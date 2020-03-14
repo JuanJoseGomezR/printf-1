fd0f6d2fcc2709da978341389a8bef82d0149716
Andres <Anhujuju1@gmail.com>
2020-03-14 16:49:10 +0000

    update holberton.h

--- holberton.h
+++ holberton.h
@@ -10,17 +10,16 @@ struct pter
 
 typedef struct pter pter_l;
 
-<<<<<<< HEAD
 int _putchar(char c);
 int write_c(char c);
 int _printf(const char *format, ...);
 
-=======
 int _printf(const char *format, ...);
 int write_c(char);
 int print_c(va_list list);
 int print_s(va_list list);
 int print_percent(va_list list);
 int print_i(va_list list);
->>>>>>> f7e4f99d1b8e03c67386f9c3ad63745b59f68418
+int print_b(va_list b);
+int print_o(va_list b);
 #endif
