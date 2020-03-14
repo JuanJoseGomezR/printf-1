552340388b4a4f5e901c8f55533e7bb688baba78
FabianMosquera <rfmq9530@gmail.com>
2020-03-14 17:46:17 -0500

    Test

--- _printf.c
+++ _printf.c
@@ -5,9 +5,9 @@
  * @format: const char
  * Return: integer
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
-	pt list[] = {
+	pter_l f_list[] = {
 		{"c", print_c},
 		{"s", print_s},
 		{"d", print_d},
@@ -21,4 +21,12 @@ int _printf(const char *format, ...);
 		{"x", print_x},
 		{"X", print_X},
 	};
+	va_list arg_list;
+	if (format == NULL)
+		return(-1);
+	va_start(arg_list, format);
+	save = validate(format, f_list, arg_list);
+	va_end(arg_list);
+
+	return (save);
 }
