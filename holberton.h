ac239304c01efd390475efc6938f162dccd5aefb
FabianMosquera <rfmq9530@gmail.com>
2020-03-14 11:36:04 -0500

    Library

--- holberton.h
+++ holberton.h
@@ -1,14 +1,19 @@
 #ifndef holberton_h
 #define holberton_h
 #include <stdarg.h>
-typedef struct pter
+struct pter
 {
     char *ptr;
     int (*ptr_int)(va_list);
 
-} pt;
+};
 
+typedef struct pter pter_l;
 
-int _putchar(char c);
-
+int _printf(const char *format, ...);
+int write_c(char);
+int print_c(va_list list);
+int print_s(va_list list);
+int print_percent(va_list list);
+int print_i(va_list list);
 #endif
