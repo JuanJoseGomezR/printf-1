c66bd88d79c9c4682dff59521e7ec099aeacaa96
FabianMosquera <rfmq9530@gmail.com>
2020-03-14 19:10:55 -0500

    Test

--- validate.c
+++ validate.c
@@ -5,15 +5,15 @@
 
 int validate(const char *format, pter_l f_list[], va_list arg_list)
 {
-    int i, j, k, k_val, print_all;
+    int i, j, k_val, print_all;
 
     for(i = 0; format[i] != '\0'; i++)
     {
         if(format[i] == '%')
         {
-            for (j = 0; f_list[j].ptr =! NULL; j++)
+            for (j = 0; f_list[j].ptr != NULL; j++)
             {
-                if (format[i] == f_list[j].ptr[0])
+                if (format[i + 1] == f_list[j].ptr[0])
                 {
                     k_val = f_list[j].ptr_int(arg_list);
                     if (k_val == -1)
@@ -25,17 +25,17 @@ int validate(const char *format, pter_l f_list[], va_list arg_list)
                 }
                 if (f_list [j].ptr == NULL && format [i] != ' ')
                 {
-                    if(format[i] != '\0')
+                    if(format[i + 1] != '\0')
                     {
                         write_c (format[i]);
                         write_c (format [i + 1]);
-
+			print_all = print_all + 2;
                     }
                     else
                     {
                         return(-1);
                     }
-                    i +=1;
+                    i = i + 1;
                 }
                 else
                 {
